56f668ea70df19a0a369d4bc5043b9c97e5ea158
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-02 12:21:33 +0000

    Started day2 and solved the example input

---
+++ Day_2/part1.js
@@ -0,0 +1,38 @@
+function part1() {
+  let total = 0;
+  const fs = require("fs");
+  const input = fs.readFileSync("Day_2/input.txt", "utf8");
+  const inputArr = input
+    .trim()
+    .split("\n")
+    .map((line) => line.trim());
+
+  const lines = inputArr.map((line) => line.split(" "));
+
+  lines.forEach((line) => {
+    let increasing = true;
+    let decreasing = true;
+    for (let i = 0; i < line.length - 1; i++) {
+      if (line[i] > line[i + 1]) {
+        increasing = false;
+      }
+      if (line[i] < line[i + 1]) {
+        decreasing = false;
+      }
+      if (line[i] === line[i + 1]) {
+        increasing = false;
+        decreasing = false;
+      }
+
+      if (Math.abs(line[i] - line[i + 1]) > 3) {
+        increasing = false;
+        decreasing = false;
+      }
+    }
+    if (increasing || decreasing) {
+      total += 1;
+    }
+  });
+  console.log(total);
+}
+part1();
