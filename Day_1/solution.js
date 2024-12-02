88831de3253a6459cdb482946b5738d9d6fb0e62
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-02 11:01:37 +0000

    Seperated the text into two columns left and right

--- Day_1/solution.js
+++ Day_1/solution.js
@@ -1,11 +1,18 @@
 function solution() {
+  const leftColumn = [];
+  const rightColumn = [];
   const fs = require("fs");
   const input = fs.readFileSync("Day_1/input.txt", "utf8");
   const lines = input
     .trim()
     .split("\n")
     .map((line) => line.trim());
-  console.log(lines[0]);
+
+  const splitInput = lines.map((line) => line.split(/\s+/).map(Number)); //splitINput maps through each line and seperates
+  splitInput.forEach((line) => {
+    leftColumn.push(line[0]), rightColumn.push(line[1]);
+  });
+  console.log(leftColumn);
 }
 
 solution();
