a73c2401d419fb5604cd711bb0778d4e8217ce65
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-02 10:42:06 +0000

    Imported the input file to work on

--- Day_1/solution.js
+++ Day_1/solution.js
@@ -1,5 +1,11 @@
 function solution() {
-  console.log("Solution for Day 1!");
+  const fs = require("fs");
+  const input = fs.readFileSync("Day_1/input.txt", "utf8");
+  const lines = input
+    .trim()
+    .split("\n")
+    .map((line) => line.trim());
+  console.log(lines[0]);
 }
 
 solution();
