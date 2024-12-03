98781353497e7429c6ac1ecae5f3c37362c9538a
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-12-03 11:40:35 +0000

    Finished part1 day 3

--- Day_3/part1.js
+++ Day_3/part1.js
@@ -1,6 +1,6 @@
 function part1() {
   const fs = require("fs");
-  const input = fs.readFileSync("inputTest.txt", "utf-8");
+  const input = fs.readFileSync("input.txt", "utf-8");
 
   const resultArr = input.match(/mul\(\d+,\d+\)/g);
 
@@ -10,10 +10,13 @@ function part1() {
 }
 
 function processCals(array) {
+  let result = 0;
   array.forEach((numbers) => {
     let numberArr = numbers.match(/\d+/g);
-    console.log(numberArr);
+    result += numberArr[0] * numberArr[1];
   });
+
+  return result;
 }
 
 part1();
